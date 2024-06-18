7263b2157c3103b0d550bc3c5912212dac7940c2
Robena Rasolond <82022919+Robe-Ras@users.noreply.github.com>
2024-06-18 13:50:11 +0200

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Menu from './assets/components/NavCircle/Menu/Menu';
 import Accueil from './assets/Pages/Accueil/Accueil';
 import Login from './assets/Pages/Login/Login';
 import Register from './assets/Pages/Register/Register';
+import ResetPassword from './assets/Pages/ResetPassword/ResetPassword';
+import ForgetPassword from './assets/Pages/ForgetPassword/ForgetPassword';
 import Footer from './assets/components/Footer/Footer';
 
 const App = () => {
@@ -13,6 +15,8 @@ const App = () => {
         <Route path="/" element={<Accueil />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/reset-password" element={<ResetPassword />} /> 
+        <Route path="/forget-password" element={<ForgetPassword />} />
       </Routes>
       <Footer />
     </Router>
@@ -20,3 +24,4 @@ const App = () => {
 };
 
 export default App;
+
